4d9c883a6f1f1dccadeab71f7413609bf25fb372
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-12 20:50:08 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical_chatbot",
+    version="0.1.0",
+    author="Vishishta Dilsara",
+    author_email="vishishtadilsara2002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
